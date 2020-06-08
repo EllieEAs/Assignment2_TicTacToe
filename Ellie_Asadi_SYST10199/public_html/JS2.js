7953b706259632f80b0640bad2c2a2ceabfcda61
Ellie Asadi <asadie@sheridancollege.ca>
2020-06-08 11:38:10 -0400

    Web_programming

---
+++ Ellie_Asadi_SYST10199/public_html/JS2.js
@@ -0,0 +1,78 @@
+const question = getElementById("question");
+const choices = getElementByClass("choice_text");
+//creating variable
+var currentQuestion = {};
+var gettingAnswer = true;
+var questionCounter = 0;
+var score = 0;
+var availableQuestion = [];
+        
+var questions = [
+        {
+        question:
+                "What is -189 + 546? ",
+                choice1 = "-357",
+                choice2 = "357",
+                choice3 = "358",
+                choice4 = "258",
+                answer = 2
+        },
+        {
+        question:
+                "What is 675 - 345?"
+                choice1 = "230",
+                choice2 = "233",
+                choice3 = "230",
+                choice4 = "343",
+                answer = 1
+        },
+        {
+        question:
+                " What is 675 - 345?"
+                choice1 = "230",
+                choice2 = "233",
+                choice3 = "230",
+                choice4 = "343",
+                answer = 1
+        },
+    ],
+            
+const = tatalQuestion =2;
+function startGame(){
+    questionCounter =0;
+    score =0;
+    availableQuestion = 
+        {
+        question:
+                "What is -189 + 546? ",
+                choice1 = "-357",
+                choice2 = "357",
+                choice3 = "358",
+                choice4 = "258",
+                answer = 2
+        },
+        {
+        question:
+                " What is 675 - 345?"
+                choice1 = "230",
+                choice2 = "233",
+                choice3 = "230",
+                choice4 = "343",
+                answer = 1
+        },
+        {
+        question:
+                " What is 675 - 345?"
+                choice1 = "230",
+                choice2 = "233",
+                choice3 = "230",
+                choice4 = "343",
+                answer = 1
+        },
+    ],
+    
+    console.log("availableQuestion")
+startGame();
+    
+}
+
